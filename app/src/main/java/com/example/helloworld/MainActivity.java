3264f6f6e8e2d1fb46c62ea669baef94e938cf68
skychx <skychx@hotmail.com>
2021-10-09 18:58:42 +0800

    迁移到 AndroidX

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.helloworld;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
